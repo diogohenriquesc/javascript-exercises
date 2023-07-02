d784ef0458bacb597183ad7cb733c55238579825
Diogo Henrique <diogohs110@gmail.com>
2023-07-02 14:46:42 -0300

    Finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const formatedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+	return formatedStr.split('').reverse().join('') === formatedStr;
 };
 
 // Do not edit below this line
