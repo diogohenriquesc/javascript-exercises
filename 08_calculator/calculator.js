7fd456d33c87409351be10ff345d2f0f2096905a
Diogo Henrique <diogohs110@gmail.com>
2023-07-02 14:34:34 -0300

    Finished exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, num) => total += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...nums) {
+  return nums.reduce((total, num) => total *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return n1 ** n2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  fat = 1;
+  for (let i = num; i > 0; i--) {
+    fat *= i;
+  }
+  return fat;
 };
 
 // Do not edit below this line
