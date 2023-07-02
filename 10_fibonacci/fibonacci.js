82aad8cd1cbcd90a46e2c5eeb0be4725afd08bf9
Diogo Henrique <diogohs110@gmail.com>
2023-07-02 15:01:15 -0300

    Finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return 'OOPS';
+	const sequence = [0, 1];
+	let first = sequence[0];
+	let second = sequence[1];
+	for (let i = 0; i < num; i++) {
+		let next = first + second;
+		first = second;
+		second = next;
+    sequence.push(next);
+	}
+  return sequence[num];
 };
 
 // Do not edit below this line
